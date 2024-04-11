e68bdcf11cbf753ad366ff5621366a90866715d6
victormmarcos <victormmarcos@hotmail.com>
2024-04-10 22:52:35 -0300

    commit 4

--- app.js
+++ app.js
@@ -1,11 +1,55 @@
-function clicar(){
+function somar(){
+
     // Entrada
     var numero1 = parseFloat(document.getElementById('numero1').value)
     var numero2 = parseFloat(document.getElementById('numero2').value)
+    var resultado = document.getElementById('resultado')
 
     // Processamento
-    var resultado = numero1 + numero2
+    var soma = numero1 + numero2
 
     // Saída
-    console.log(resultado)
+    resultado.innerText = `O resultado da soma é ${soma}`
+}
+
+function subtrair(){
+
+    // Entrada
+    var numero1 = parseFloat(document.getElementById('numero1').value)
+    var numero2 = parseFloat(document.getElementById('numero2').value)
+    var resultado = document.getElementById('resultado')
+
+    // Processamento
+    var soma = numero1 - numero2
+
+    // Saída
+    resultado.innerText = `O resultado da subtração é ${soma}`
+}
+
+function multiplicar(){
+
+    // Entrada
+    var numero1 = parseFloat(document.getElementById('numero1').value)
+    var numero2 = parseFloat(document.getElementById('numero2').value)
+    var resultado = document.getElementById('resultado')
+
+    // Processamento
+    var soma = numero1 * numero2
+
+    // Saída
+    resultado.innerText = `O resultado da multiplicação é ${soma}`
+}
+
+function dividir(){
+
+    // Entrada
+    var numero1 = parseFloat(document.getElementById('numero1').value)
+    var numero2 = parseFloat(document.getElementById('numero2').value)
+    var resultado = document.getElementById('resultado')
+
+    // Processamento
+    var soma = numero1 / numero2
+
+    // Saída
+    resultado.innerText = `O resultado da divisão é ${soma}`
 }
