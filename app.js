bc242cb0115038329f7c01526dbc36f0eeb4ff79
victormmarcos <victormmarcos@hotmail.com>
2024-04-10 22:24:13 -0300

    Commit3

---
+++ app.js
@@ -0,0 +1,11 @@
+function clicar(){
+    // Entrada
+    var numero1 = parseFloat(document.getElementById('numero1').value)
+    var numero2 = parseFloat(document.getElementById('numero2').value)
+
+    // Processamento
+    var resultado = numero1 + numero2
+
+    // Saída
+    console.log(resultado)
+}
